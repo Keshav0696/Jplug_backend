31cd2446d1a6cef55bc0bf4a2d4b70d55c50167a
keshavgarg <keshavgarg0696@gmail.com>
2020-12-26 16:44:06 +0530

    commit changes

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ var authRouter = require('./routes/auth');
 var cors = require('cors');
 
 var app = express();
+var debug = require('debug')('express-generator:server');
+
+var server = require('http').createServer(app);
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -122,7 +128,67 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+server.listen(port, function(err){
+  if(!err) console.log(port)
+  });
+  server.on('error', onError);
+  server.on('listening', onListening);
 
+  function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+
+  /**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 module.exports = app;
 
 
